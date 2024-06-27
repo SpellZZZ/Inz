b51908775bb1f0b43c98cc2970ca25fcc9174e2a
SpellZZZ <james321321@wp.pl>
2024-06-27 17:19:46 +0200

    FIX interfaces fill

--- src/main/java/org/example/service/managementService/CommissionManagementService.java
+++ src/main/java/org/example/service/managementService/CommissionManagementService.java
@@ -1,4 +1,14 @@
 package org.example.service.managementService;
 
+import org.example.dto.CommissionDto;
+import org.example.model.Address;
+
 public interface CommissionManagementService {
+    public void commissionCreate(CommissionDto commissionDto,
+                                 Address addressStart,
+                                 Address addressEnd,
+                                 String authorizationHeader);
+    public Address addressStartCreate(CommissionDto commissionDto);
+    public Address addressEndCreate(CommissionDto commissionDto);
+
 }
--- src/main/java/org/example/service/managementService/RouteManagementService.java
+++ src/main/java/org/example/service/managementService/RouteManagementService.java
@@ -1,4 +1,16 @@
 package org.example.service.managementService;
 
+import org.example.dto.RouteAddDto;
+import org.example.dto.RouteDataDto;
+import org.example.dto.RouteDto;
+import org.example.model.Route;
+import org.example.model.User;
+
+import java.util.List;
+
 public interface RouteManagementService {
+    public void createRoute(RouteAddDto routeAddDto, User user);
+    public List<RouteDto> getRoutes(User user);
+    public RouteDataDto getRouteDate(Route route, User user);
+
 }
